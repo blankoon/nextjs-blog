9c20a3244e502c14a6210228ac876bdb833d1d29
blankoon <clat.dev@gmail.com>
2021-07-17 21:40:59 +0900

    🩹 Replace single quote to `&apos;`

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm blankoon who likes blankets the most in the world 🛌</p>
+        <p>Hello, I&apos;m blankoon who likes blankets the most in the world 🛌</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
