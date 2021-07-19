8a96ac956499520670cf74eb2ca9a4f48c54235f
blankoon <clat.dev@gmail.com>
2021-07-19 23:26:05 +0900

    ✨ Implement dynamic routes for posts by filenames
    - Add `getAllPostIds` for `getStaticPaths`
    https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticpaths
    step 4
    
    - Add `getPostData` for `getStaticProps`
    https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops
    step 5

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,43 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsResult } from 'next'
+import { PostData, getAllPostIds, getPostData } from '../../lib/posts'
+
+import Layout from '../../components/layout'
+import { ParsedUrlQuery } from 'querystring'
+
+type PostProps = {
+  postData: PostData;
+};
+
+export default function Post({ postData }: PostProps) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+interface IParams extends ParsedUrlQuery {
+  id: string
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { id } = context.params as IParams
+  const postData = getPostData(id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
